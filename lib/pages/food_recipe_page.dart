1144b749e801b409c6c06930609f19351b7b2959
PardusDev <rustemavci@outlook.com.tr>
2023-10-06 14:48:58 +0300

    Food Recipe Page finished.

--- lib/pages/food_recipe_page.dart
+++ lib/pages/food_recipe_page.dart
@@ -1,4 +1,11 @@
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
 import 'package:flutter/material.dart';
+
+List<String> images = [
+  'https://i.ibb.co/D9Sj6WR/image.png',
+  'https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg?auto=compress&cs=tinysrgb&w=350&h=350&dpr=1',
+  'https://images.pexels.com/photos/8969237/pexels-photo-8969237.jpeg?auto=compress&cs=tinysrgb&w=350&h=350&dpr=1',
+];
 
 class FoodRecipePage extends StatefulWidget {
   const FoodRecipePage({super.key});
@@ -13,7 +20,11 @@ class _FoodRecipePageState extends State<FoodRecipePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Center(child: Text("Food Recipes", style: TextStyle(color: Colors.black),)),
+        title: Center(
+            child: Text(
+          "Food Recipes",
+          style: TextStyle(color: Colors.black),
+        )),
         backgroundColor: Colors.white,
         shadowColor: Colors.transparent,
         leading: Padding(
@@ -24,8 +35,11 @@ class _FoodRecipePageState extends State<FoodRecipePage> {
               shape: BoxShape.circle,
             ),
             child: IconButton(
-              onPressed: () {  },
-              icon: Icon(Icons.chevron_left, color: Colors.black,),
+              onPressed: () {},
+              icon: Icon(
+                Icons.chevron_left,
+                color: Colors.black,
+              ),
             ),
           ),
         ),
@@ -39,42 +53,104 @@ class _FoodRecipePageState extends State<FoodRecipePage> {
               ),
               child: IconButton(
                 icon: Icon(Icons.favorite_border, color: Colors.black),
-                onPressed: () {
-
-                },
+                onPressed: () {},
               ),
             ),
-          )
+          ),
         ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(18.0),
-          child: Column(
-            children: [
-              Center(child: FractionallySizedBox(widthFactor: 0.7, child: Image.network('https://i.ibb.co/D9Sj6WR/image.png', fit: BoxFit.cover))),
-              SizedBox(height: 30,),
-              Text("Savor the Aromas: Esquisite Exotic Spice Infusion Rice Bowl", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),),
-              SizedBox(height: 7,),
-              Row(
-                children: [
-                  Text("By ", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w600),),
-                  Text("Anindya Rahma", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),)
-                ],
+          child: Column(children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: 250),
+              child: Swiper(
+                itemBuilder: (context, index) {
+                  return Image.network(images[index], fit: BoxFit.cover,);
+                },
+                itemCount: images.length,
+                pagination: SwiperPagination(
+                  builder: DotSwiperPaginationBuilder(
+                    color: Colors.white38,
+                    activeColor: Colors.white
+                  )
+                ),
+                viewportFraction: 0.82,
+                scale: 0.85,
               ),
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextWithIcon(icon: Icon(Icons.alarm), text: "15 Mins",),
-                  TextWithIcon(icon: Icon(Icons.rice_bowl_outlined), text: "21 Ingredients",),
-                  TextWithIcon(icon: Icon(Icons.soup_kitchen_outlined), text: "2 Servings",),
-                ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Savor the Aromas: Esquisite Exotic Spice Infusion Rice Bowl",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            Row(
+              children: [
+                Text(
+                  "By ",
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  "Anindya Rahma",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextWithIcon(
+                  icon: Icon(Icons.alarm),
+                  text: "15 Mins",
+                ),
+                TextWithIcon(
+                  icon: Icon(Icons.rice_bowl_outlined),
+                  text: "21 Ingredients",
+                ),
+                TextWithIcon(
+                  icon: Icon(Icons.soup_kitchen_outlined),
+                  text: "2 Servings",
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Divider(),
+            SizedBox(height: 15),
+            Text(
+              "This dish features fragrant jasmine rice meticulously infused with a blend of rare and aromatic spices. This dish features fragrant jasmine rice meticulously",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: double.infinity,
+              child: MaterialButton(
+                height: 50,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Color.fromRGBO(34, 34, 34, 1.0),
+                child: Text(
+                  "Start Cooking",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                onPressed: () {},
               ),
-              SizedBox(height: 10,),
-              Divider(),
-            ]
-          ),
+            )
+          ]),
         ),
       ),
     );
@@ -89,16 +165,12 @@ class TextWithIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        this.icon,
-        SizedBox(width: 10,),
-        Text(
-          this.text,
-          style: TextStyle(color: Colors.black)
-        )
-      ]
-    );
+    return Row(children: [
+      this.icon,
+      SizedBox(
+        width: 10,
+      ),
+      Text(this.text, style: TextStyle(color: Colors.black))
+    ]);
   }
 }
-
